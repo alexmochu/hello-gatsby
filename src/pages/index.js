c39918a92a55d82bf49eb4e293a0e18205384e8c
alexmochu <mochualex4@gmail.com>
2018-10-07 16:39:05 +0300

    Get to know Gatsby building blocks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello Gatsby!</div>
+export default () => 
+    <div style={{ color: `purple` }}>
+        <Link to="/contact/">contact</Link>
+        <Link to="/about/"> about</Link>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        {/* <PrimaryButton>Click me</PrimaryButton> */}
+    </div>
